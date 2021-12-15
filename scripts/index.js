691bbb6f0f64651e054ba1f0d637dbc07205abff
Yury Smirnov <appleswift@yandex.ru>
2021-12-15 22:32:56 +0300

    Рефакторинг кода popup окна по замечаниям ревьюера от 15122021

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,19 @@
-let popupEditProfileForm = document.querySelector(".popup-container");
-let popupCloseButton = popupEditProfileForm.querySelector(
-  ".popup__button-close");
+let popupEditProfile = document.querySelector(".popup");
+let popupCloseButton = popupEditProfile.querySelector(".popup__button-close");
 
-let nameInputField = popupEditProfileForm.querySelector("#profile-name-field");
-let descriptionInputField = popupEditProfileForm.querySelector("#profile-description-field");
+let nameInputField = popupEditProfile.querySelector("#profile-name-field");
+let descriptionInputField = popupEditProfile.querySelector("#profile-description-field");
 let profileSection = document.querySelector(".profile");
 let profileName = profileSection.querySelector(".profile__info-author");
-let profileDescription = profileSection.querySelector(
-  ".profile__info-description"
-);
+let profileDescription = profileSection.querySelector(".profile__info-description");
 let profileEditButton = profileSection.querySelector(".profile__button-edit");
 function handleEditButton() {
-  popupEditProfileForm.classList.add("popup_opened");
+  popupEditProfile.classList.add("popup_opened");
   nameInputField.value = profileName.textContent;
   descriptionInputField.value = profileDescription.textContent;
 }
 function handlePopupCloseButton() {
-  popupEditProfileForm.classList.remove("popup_opened");
+  popupEditProfile.classList.remove("popup_opened");
 }
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -29,4 +26,4 @@ function formSubmitHandler(evt) {
 //"слушатели" событий
 profileEditButton.addEventListener("click", handleEditButton);
 popupCloseButton.addEventListener("click", handlePopupCloseButton);
-popupEditProfileForm.addEventListener("submit", formSubmitHandler);
+popupEditProfile.addEventListener("submit", formSubmitHandler);
